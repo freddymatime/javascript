f3ebb7aef989801c82efce01628d489ba4ea1231
freddymatime <freddy.h.matime@gmail.com>
2019-09-20 16:00:09 +0200

    ajout exercice space runner

--- dossierAtelecharger/questions.js
+++ dossierAtelecharger/questions.js
@@ -4,40 +4,51 @@
  * Il peut y avoir plusieur façon de faire.
  */
 var tailleString = function (texte) {
-
+return texte.length;
 }
 var remplaceECar = function (texte) {
+    return texte.replace("e"," ");
 
 }
-var concatString = function (texte1, texte2) {
 
+var concatString = function (texte1, texte2) {
+   return texte1 + texte2;
 }
 var afficherCar5 = function (texte) {
-
+return texte[4];
 }
 var afficher9Car = function (texte) {
+    return texte.substring([0],[9]);
 
 }
 var majusculeString = function (texte) {
-
+return texte.toUpperCase();
 }
 var minusculeString = function (texte) {
+    return texte.toLowerCase()
 
 }
 var SupprEspaceString = function (texte) {
+    return texte.trim();
 
 }
 var IsString = function (texte) {
+    if(typeof(texte)=="string"){
+        return true
+    }
+
 
 }
 
 var AfficherExtensionString = function (texte) {
+    return texte.split('.').pop();
 
 }
 var NombreEspaceString = function (texte) {
-
+    return texte.split(' ').length - 1;
 }
 var InverseString = function (texte) {
+    return texte.split("").reverse().join("");
 
 }
 
@@ -45,20 +56,32 @@ var InverseString = function (texte) {
  * Exercices sur les nombres et les caluls mathématiques
  */
 var calculPuissance = function (x, y) {
-
+ return Math.pow(2,3);
 }
 var valeurAbsolue = function (nombre) {
+    return Math.abs(nombre);
 
 }
 var valeurAbsolueArray = function (array) {
-
+   for (i=0; i<array.length; i++){
+       array[i] = Math.abs(array[i]);
+       
+   }
+   return array;
+    
 }
 var sufaceCercle = function (rayon) {
+    return Math.round( Math.pow(rayon,2)*Math.PI);
 
 }
 var hypothenuse = function (ab, ac) {
+    return Math.hypot(ab,ac);
 
 }
 var calculIMC = function (poids, taille) {
+    
+        calculIMC= parseFloat((poids/(taille*taille)).toFixed(2))
+    return calculIMC
 
+    
 }
---
+++ serpent/script.js
@@ -0,0 +1,33 @@
+window.onload = function()
+{
+    var canvas;
+    var ctx;
+    var delay = 100;
+    var xCoord = 0;
+    var yCoord = 0;
+
+    init();
+
+    function init()
+        {
+            canvas = document.createElement("canvas");
+            canvas.width =900;
+            canvas.height = 600;
+            canvas.style.border="10px solid";
+            document.body.appendChild(canvas);
+            ctx = canvas.getContext("2d");
+            refreshCanvas();
+
+            
+        }
+    function refreshCanvas()   
+        {
+            xCoord+=10;
+            yCoord+=2;
+            ctx.clearRect(0,0,canvas.width, canvas.height);
+            ctx.fillStyle ="red";
+            ctx.fillRect (xCoord , yCoord, 100, 50);
+            setTimeout(refreshCanvas,delay);
+        } 
+        
+}
